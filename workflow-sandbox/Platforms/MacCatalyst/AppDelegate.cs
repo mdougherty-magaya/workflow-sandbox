5514c19368ca8d569bda50677c987aff993fe87a
Mike Dougherty <mdougherty@magaya.com>
2024-05-02 12:14:19 -0400

    add basic MAUI app from template

---
+++ workflow-sandbox/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace workflow_sandbox;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
